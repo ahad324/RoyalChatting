01fa94e0cbf9cbc80e1173990eb5a9938e5adf88
ahad <ahadg446@gmail.com>
2024-03-03 03:00:15 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,82 @@
+document.addEventListener("DOMContentLoaded", function (e) {
+  const socket = io("http://localhost:8000");
+  const messagecontainer = document.querySelector(".container");
+  const form = document.getElementById("send-container");
+  const messageinput = document.getElementById("messageinp");
+  const currentmembers = document.getElementById("CurrentMembers");
+  let audio = new Audio("./notification.mp3");
+  const scrollToBottom = () => {
+    messagecontainer.scrollTop = messagecontainer.scrollHeight;
+  };
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const message = messageinput.value;
+    append(`You : ${message}`, "messageSent");
+    socket.emit("send", message);
+    messageinput.value = "";
+  });
+
+  //   let username = prompt("Enter Your Name:");
+  const savedname = localStorage.getItem("name");
+  console.log("Saved name: " + savedname);
+  let username;
+  if (!savedname || savedname === "null") {
+    username = prompt("Enter Your Name:");
+    localStorage.setItem("name", username);
+  } else {
+    username = savedname;
+  }
+  socket.emit("new-user-joined", username);
+
+  const members = {};
+
+  const updateMembersList = (memberList) => {
+    currentmembers.innerHTML = "";
+    memberList.forEach((member) => {
+      const memberElement = document.createElement("li");
+      memberElement.textContent = member;
+      currentmembers.appendChild(memberElement);
+    });
+  };
+
+  const append = (message, position) => {
+    const messageElement = document.createElement("div");
+    messageElement.innerHTML = message;
+    messageElement.classList.add("message");
+    messageElement.classList.add(position);
+   messageElement.classList.add('animation');
+
+   messagecontainer.appendChild(messageElement);
+
+   messageElement.style.transition = 'transform .1ms ease';
+   setTimeout(() => {
+       messageElement.classList.remove('animation');
+   }, 1000);
+
+    if (position === "messageReceived") {
+      audio.play();
+    }
+    scrollToBottom();
+  };
+  socket.on("user-joined", (user) => {
+    append(`${user} joined the chat`, "Joined");
+    members[user] = user;
+    updateMembersList(Object.values(members));
+  });
+  socket.on("recieve", (data) => {
+    append(`${data.name} : ${data.message}`, "messageReceived");
+  });
+  socket.on("leave", (user) => {
+    append(`${user} left the chat`, "leave");
+    delete members[user];
+    updateMembersList(Object.values(members));
+  });
+
+  socket.on("current-members", (currentMembers) => {
+    currentMembers.forEach((member) => {
+      members[member] = member;
+    });
+    updateMembersList(currentMembers);
+  });
+});
---
+++ server/server.js
@@ -0,0 +1,26 @@
+const io = require("socket.io")(8000, {
+  cors: {
+    origin: "http://127.0.0.1:5500",
+    methods: ["GET", "POST"],
+  },
+});
+
+const users = {};
+io.on("connection", (socket) => {
+  socket.on("new-user-joined", (name) => {
+    console.log("New User Connected ", name);
+    users[socket.id] = name;
+    socket.broadcast.emit("user-joined", name);
+    socket.emit("current-members", Object.values(users));
+  });
+  socket.on("send", (message) => {
+    socket.broadcast.emit("recieve", {
+      message: message,
+      name: users[socket.id],
+    });
+  });
+  socket.on("disconnect", (message) => {
+    socket.broadcast.emit("leave", users[socket.id]);
+    delete users[socket.id];
+  });
+});
